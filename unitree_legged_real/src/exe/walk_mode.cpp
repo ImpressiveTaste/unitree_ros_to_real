f534bd1bddfafdf5e66f205d41bcd761a90b1b38
ImpressiveTaste <94606213+ImpressiveTaste@users.noreply.github.com>
2022-02-16 17:19:06 +0100

    commented walk mode example 3.2 (Aliengo)
    commented walk mode example, code used with an aliengo robot
    
    before catkin_make set (in shell) the following:
    export UNITREE_SDK_VERSION=3_2
    export UNITREE_LEGGED_SDK_PATH=~/unitree_legged_sdk
    export UNITREE_PLATFORM="arm32"

--- unitree_legged_real/src/exe/walk_mode.cpp
+++ unitree_legged_real/src/exe/walk_mode.cpp
@@ -12,7 +12,12 @@ Use of this source code is governed by the MPL-2.0 license, see LICENSE.
 #include <unitree_legged_msgs/HighState.h>
 #include "convert.h"
 
+#ifdef SDK3_1
+using namespace aliengo;
+#endif
+#ifdef SDK3_2
 using namespace UNITREE_LEGGED_SDK;
+#endif
 
 template<typename TLCM>
 void* update_loop(void* param)
@@ -51,86 +56,75 @@ int mainHelper(int argc, char *argv[], TLCM &roslcm)
         motiontime = motiontime+2;
         roslcm.Get(RecvHighLCM);
         RecvHighROS = ToRos(RecvHighLCM);
+        // printf("%f\n",  RecvHighROS.forwardSpeed); //used if we want to get feedback from robot
 
-        SendHighROS.mode = 0;      
-        SendHighROS.gaitType = 0;
-        SendHighROS.speedLevel = 0;
-        SendHighROS.footRaiseHeight = 0;
-        SendHighROS.bodyHeight = 0;
-        SendHighROS.euler[0]  = 0;
-        SendHighROS.euler[1] = 0;
-        SendHighROS.euler[2] = 0;
-        SendHighROS.velocity[0] = 0.0f;
-        SendHighROS.velocity[1] = 0.0f;
-        SendHighROS.yawSpeed = 0.0f;
-        SendHighROS.reserve = 0;
+        //default positions
+        SendHighROS.forwardSpeed = 0.0f;
+        SendHighROS.sideSpeed = 0.0f;
+        SendHighROS.rotateSpeed = 0.0f;
+        SendHighROS.bodyHeight = 0.0f;
 
-        if(motiontime > 0 && motiontime < 1000){
-            SendHighROS.mode = 1;
-            SendHighROS.euler[0] = -0.3;
+        SendHighROS.mode = 0;
+        SendHighROS.roll  = 0;
+        SendHighROS.pitch = 0;
+        SendHighROS.yaw = 0;
+
+        if(motiontime>1000 && motiontime<1500){ //after one second for half a second it will send standing mode
+            SendHighROS.mode = 1; //standing mode
+            // SendHighROS.roll = 0.3f;
         }
-        if(motiontime > 1000 && motiontime < 2000){
-            SendHighROS.mode = 1;
-            SendHighROS.euler[0] = 0.3;
+
+        if(motiontime>1500 && motiontime<2000){ //after 1.5seconds for half a second will do
+            SendHighROS.mode = 1; //standing mode
+            SendHighROS.pitch = 0.3f; //robot nose points up at 6degrees
         }
-        if(motiontime > 2000 && motiontime < 3000){
-            SendHighROS.mode = 1;
-            SendHighROS.euler[1] = -0.2;
+
+        if(motiontime>2000 && motiontime<2500){ //after 2 seconds for half a second
+            SendHighROS.mode = 1; //standing mode
+            SendHighROS.yaw = 0.2f; //yaw of 5.6degrees (twisted to the right)
         }
-        if(motiontime > 3000 && motiontime < 4000){
-            SendHighROS.mode = 1;
-            SendHighROS.euler[1] = 0.2;
+
+        if(motiontime>2500 && motiontime<3000){
+            SendHighROS.mode = 1; //standing mode
+            SendHighROS.bodyHeight = -0.3f; //set body height to 0.377m (lowers robot)
         }
-        if(motiontime > 4000 && motiontime < 5000){
-            SendHighROS.mode = 1;
-            SendHighROS.euler[2] = -0.2;
+
+        if(motiontime>3000 && motiontime<3500){
+            SendHighROS.mode = 1; //standing mode
+            SendHighROS.bodyHeight = 0.3f; //set body height to 0.422m (lift robot height)
         }
-        if(motiontime > 5000 && motiontime < 6000){
-            SendHighROS.mode = 1;
-            SendHighROS.euler[2] = 0.2;
+
+        if(motiontime>3500 && motiontime<4000){
+            SendHighROS.mode = 1; //standing mode
+            SendHighROS.bodyHeight = 0.0f; //set body height to default
         }
-        if(motiontime > 6000 && motiontime < 7000){
-            SendHighROS.mode = 1;
-            SendHighROS.bodyHeight = -0.2;
+
+        if(motiontime>4000 && motiontime<5000){
+            SendHighROS.mode = 2; //set robot to walking mode
         }
-        if(motiontime > 7000 && motiontime < 8000){
-            SendHighROS.mode = 1;
-            SendHighROS.bodyHeight = 0.1;
+
+        if(motiontime>5000 && motiontime<8500){ //3.5seconds
+            SendHighROS.mode = 2; //walking mode
+            SendHighROS.forwardSpeed = 0.1f; // -1  ~ +1 ; go forward at a speed of 0.08m/s so does 28cm 
         }
-        if(motiontime > 8000 && motiontime < 9000){
-            SendHighROS.mode = 1;
-            SendHighROS.bodyHeight = 0.0;
+
+        if(motiontime>8500 && motiontime<12000){ //3.5seconds
+            SendHighROS.mode = 2; //walking mode
+            SendHighROS.forwardSpeed = -0.2f; // -1  ~ +1; go backwards at a speed of 0.1m/s so does 35cm
         }
-        if(motiontime > 9000 && motiontime < 11000){
-            SendHighROS.mode = 5;
+
+        if(motiontime>12000 && motiontime<16000){//4seconds
+            SendHighROS.mode = 2; //walking mode
+            SendHighROS.rotateSpeed = 0.1f; //turn right 5degrees
         }
-        if(motiontime > 11000 && motiontime < 13000){
-            SendHighROS.mode = 6;
+
+        if(motiontime>16000 && motiontime<20000){//4seconds
+            SendHighROS.mode = 2; //walking mode
+            SendHighROS.rotateSpeed = -0.1f; //turn left 5degrees
         }
-        if(motiontime > 13000 && motiontime < 14000){
-            SendHighROS.mode = 0;
-        }
-        if(motiontime > 14000 && motiontime < 18000){
-            SendHighROS.mode = 2;
-            SendHighROS.gaitType = 2;
-            SendHighROS.velocity[0] = 0.4f; // -1  ~ +1
-            SendHighROS.yawSpeed = 2;
-            SendHighROS.footRaiseHeight = 0.1;
-            // printf("walk\n");
-        }
-        if(motiontime > 18000 && motiontime < 20000){
-            SendHighROS.mode = 0;
-            SendHighROS.velocity[0] = 0;
-        }
-        if(motiontime > 20000 && motiontime < 24000){
-            SendHighROS.mode = 2;
-            SendHighROS.gaitType = 1;
-            SendHighROS.velocity[0] = 0.2f; // -1  ~ +1
-            SendHighROS.bodyHeight = 0.1;
-            // printf("walk\n");
-        }
-        if(motiontime>24000 ){
-            SendHighROS.mode = 1;
+
+        if(motiontime>20000 && motiontime<21000){ //1seconds
+            SendHighROS.mode = 1; //standing mode
         }
 
         SendHighLCM = ToLcm(SendHighROS, SendHighLCM);
@@ -143,8 +137,27 @@ int mainHelper(int argc, char *argv[], TLCM &roslcm)
 
 int main(int argc, char *argv[]){
     ros::init(argc, argv, "walk_ros_mode");
+    std::string firmwork;
+    ros::param::get("/firmwork", firmwork);
 
-    UNITREE_LEGGED_SDK::LCM roslcm(UNITREE_LEGGED_SDK::HIGHLEVEL);
-    mainHelper<UNITREE_LEGGED_SDK::HighCmd, UNITREE_LEGGED_SDK::HighState, UNITREE_LEGGED_SDK::LCM>(argc, argv, roslcm);
+    #ifdef SDK3_1
+        aliengo::Control control(aliengo::HIGHLEVEL);
+        aliengo::LCM roslcm;
+        mainHelper<aliengo::HighCmd, aliengo::HighState, aliengo::LCM>(argc, argv, roslcm);
+    #endif
+
+    #ifdef SDK3_2
+        std::string robot_name;
+        UNITREE_LEGGED_SDK::LeggedType rname;
+        ros::param::get("/robot_name", robot_name);
+        if(strcasecmp(robot_name.c_str(), "A1") == 0)
+            rname = UNITREE_LEGGED_SDK::LeggedType::A1;
+        else if(strcasecmp(robot_name.c_str(), "Aliengo") == 0)
+            rname = UNITREE_LEGGED_SDK::LeggedType::Aliengo;
+
+        // UNITREE_LEGGED_SDK::InitEnvironment();
+        UNITREE_LEGGED_SDK::LCM roslcm(UNITREE_LEGGED_SDK::HIGHLEVEL);
+        mainHelper<UNITREE_LEGGED_SDK::HighCmd, UNITREE_LEGGED_SDK::HighState, UNITREE_LEGGED_SDK::LCM>(argc, argv, roslcm);
+    #endif
     
 }
